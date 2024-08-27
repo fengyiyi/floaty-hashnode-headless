e4bc1175950326947203d5ac124dad2d775742e7
Fred <fengyiyi@users.noreply.github.com>
2024-08-27 16:46:15 +0800

    Update ContactMe.tsx

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -11,18 +11,12 @@ export const ContactMe = () => {
   return (
     <footer className='mb-20 flex w-full flex-col items-center gap-5 rounded-3xl bg-white p-6 text-slate-950 shadow-lg md:p-8 dark:border dark:border-slate-800 dark:bg-slate-900 dark:text-zinc-300'>
       <h2 className='text-xl font-semibold lg:text-2xl'>
-        Have a Project Idea?
+        Hi,朋友
       </h2>
       <div className='flex flex-col items-center gap-2'>
-        <p className='text-base lg:text-lg'>Let&apos;s connect</p>
+        <p className='text-base lg:text-lg'>通过下面方式联系我</p>
         <SocialLinks />
       </div>
-      <p className='text-sm text-slate-500 dark:text-zinc-300'>
-        Template by{' '}
-        <a href='//twitter.com/iammarmirza' target='_blank'>
-          Ammar Mirza
-        </a>
-      </p>
       {data?.publication?.title && (
         <p className='flex items-center gap-1 text-sm text-slate-500 dark:text-zinc-300'>
           &copy; {data.publication.title} 2024
